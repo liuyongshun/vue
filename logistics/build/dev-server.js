cf9fdb331354857b9e5d65c63af74a3dd4f211d8
liuyongshun <liuys0902@gmail.com>
2017-07-26 20:06:12 +0800

    working end

--- logistics/build/dev-server.js
+++ logistics/build/dev-server.js
@@ -1,6 +1,8 @@
 require('./check-versions')()
 
 var config = require('../config')
+
+console.log(process.path)
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
 }
